75d8cc493dd45d39a9b8d8fec65b966e95fa3c13
krishna <109722847+sniperlyf@users.noreply.github.com>
2024-07-25 11:09:15 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,57 @@
+import java.util.*;
+public class Main       // why static in below line? because we can to run main function witout creating a class of 'Main'( i.e. name of file) class
+{
+	public static void main(String[] args) {
+// 		System.out.print("enter number: ");
+// 		Scanner sc = new Scanner(System.in);
+// 		int n = sc.nextInt();
+// 		System.out.print(n);
+		
+		//type casting ( to convert teh int to float as it automatically converts float to int
+// 		int num = (int)(67.238938f);
+// 		System.out.println(num);
+		
+		//automatic type promtion in expression
+	   // int a = 257;//byte can store max 256 int
+	   // byte b = (byte)(a);// 257 % 256 = 1 (that's what its doing)
+	   // System.out.println(b);
+	   
+	   //byte a =40;
+	   //byte b = 50;
+	   //byte c = 100;
+	   //int d = (a*b)/c; // it is an byte ivaluation so it auto. converts answer in byte but if we multiply a or b or c by 2(int) then it gives error.
+	   //System.out.println(d);
+	    
+	    
+	    //CONDITIONS------------------------------------- CONDITIONS-------------------------------------
+	   
+	   //int a = 10;
+	   //if(a == 10){
+	   //    System.out.println("yes it is 10");
+	   //}
+	   
+	   
+	   //------------WHILE LOOP------------------
+	   //int count = 1;
+	   //while (count != 5){
+	   //    System.out.println(count);
+	   //    count++;
+	   //}
+	   
+	   ////--------------FOR LOOP----------------------
+	   //for(int i = 0 ; i<6 ; i++){
+	   //    System.out.println(i);
+	   //}
+	    
+	   // ----------BASIC QUESTION----------------
+	   
+	   Scanner in = new Scanner(System.in);
+	   System.out.print("enter your temp in celcius: ");
+	   float tempC = in.nextFloat();
+	   
+	   float tempF = (tempC* 9/5) + 32;
+	   System.out.print("your temp in faranhight is: ");
+	   System.out.print(tempF);
+	}
+}
+ 
