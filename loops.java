2a7066456b496423ca703d099c23cf65d1c92bb9
sniperfye <sniperlyfe564@gmail.com>
2024-08-05 17:10:43 +0530

    new files

---
+++ loops.java
@@ -0,0 +1,84 @@
+
+
+
+
+
+
+public class loops {
+    public static void main(String[] args) {
+        // Input
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter your name: ");
+        // String name = sc.next();
+        // System.out.println("Hello, " + name);
+
+        //--------------------------------------------- conditional ---------------------------------------
+        /*      1. even-odd                     */
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("enter number: ");
+        // int x = sc.nextInt();
+
+        // if(x%2==0){
+        //     System.out.println("even");
+        // }
+        // else{
+        //     System.out.println("odd");
+        // }
+
+
+        /*       2. equal or not                */
+
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("enter a: ");
+    //     int a = sc.nextInt();
+    //     System.out.println("enter b: ");
+    //     int b = sc.nextInt();
+
+    //     if(a==b){
+    //         System.out.print("equal");
+    //     }
+    //     else if(a<b){
+    //         System.out.print("b is greater than a");
+    //     }
+    //     else{
+    //         System.out.print("a is greater than b");
+    //     }
+    // }
+
+    /*          3. switch                       */
+
+    // Scanner sc = new Scanner(System.in);
+    // System.out.println("enter day in number: ");
+    // int week = sc.nextInt();
+    // switch(week){
+    //     case 1: System.out.println("monday");
+    //     break;
+    //     case 2: System.out.println("tuesday");
+    //     break;
+    //     case 3: System.out.println("wednesday");
+    //     break;
+    //     case 4: System.out.println("thrusday");
+    //     break;
+    //     case 5: System.out.println("friday");
+    //     break;
+    //     case 6: System.out.println("saturday");
+    //     break;
+    //     case 7: System.out.println("sunday");
+    //     break;
+    // }
+
+
+    /*-----------------------------------------------LOOPS---------------------------------------------------------------- */
+    /*            1. Sum of n natural number                   */
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("enter number: ");
+        // int n = sc.nextInt();
+        // int sum=0;
+        // for(int i=1 ; i<n+1 ; i++){
+        //     sum = sum + i;
+        // }
+        // System.out.println("sum of n number is: ");
+        // System.out.println(sum);
+}
+}
