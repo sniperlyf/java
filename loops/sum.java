fe46397c9ed65b751b858f35dbe0a7252ad94986
sniperfye <sniperlyfe564@gmail.com>
2024-09-18 11:42:39 +0530

    java update

---
+++ loops/sum.java
@@ -0,0 +1,36 @@
+
+// import java.util.Scanner;
+
+// public class sum {
+//     public static void main(String[] args) {
+//         //sum(); 
+//         int ans = sum2();
+//     }
+
+//     // static void sum(){
+//     //     Scanner in = new Scanner(System.in);
+//     //     int num1, num2, sum;
+//     //     System.out.print("enter number 1: ");
+//     //     num1 = in.nextInt();
+//     //     System.out.print("enter number 2: ");
+//     //     num2 = in.nextInt();
+//     //     sum = num1 + num2;
+//     //     System.out.println("the sum is = " + sum);
+//     //     in.close();
+
+//     // }
+
+//     static int sum2(){
+//         Scanner in = new Scanner(System.in);
+//         int num1, num2, sum;
+//         System.out.print("enter number 1: ");
+//         num1 = in.nextInt();
+//         System.out.print("enter number 2: ");
+//         num2 = in.nextInt();
+//         sum = num1 + num2;
+//         System.out.println("the sum is = " + sum);
+//         in.close();
+//         return sum;
+
+//     }
+// }
